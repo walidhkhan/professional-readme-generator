72e18cc3a9e463de4932d22118276716692893a0
walidhkhan <walidhkhan@gmail.com>
2021-08-09 16:25:32 -0700

    obj destructuring issues

--- src/readme-template.js
+++ src/readme-template.js
@@ -23,4 +23,36 @@ ${name}
 };
 
 // to allow 'generateReadMe' to be called from index.js
-module.exports = generateReadMe;
+// module.exports = generateReadMe;
+module.exports = templateData => {
+    console.log(templateData);
+    // destructure from templateData based on their property key names
+    const { readmeToc, readme, ...header } = templateData;
+return `
+# Project title ${header.githubUsername}
+Project repository: ${header.repo}
+
+# Description
+${readme.description}
+
+# Table of Contents
+
+# Installation
+${readme.installation}
+
+# Usage
+${readme.usage}
+
+# License       
+${readme.license}
+
+# Contributing
+${readme.contributing} 
+
+# Tests
+${readme.tests}
+
+# Questions
+For any further questions or concerns regarding the project, please contact ${header.email}.
+`;    
+}
